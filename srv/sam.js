bb291686d6d8d0c8eb88d1be85e323b701a592e2
Sujit Kumar <skumar@convergentis.com>
2023-06-13 11:46:02 +0000

    filelocationcode

--- srv/sam.js
+++ srv/sam.js
@@ -1,21 +1,67 @@
+const XLSX = require('xlsx');
+
+
 module.exports = cds.service.impl(async function () {
-    const XLSX = require('xlsx');
+    this.on("READ", "AccrualsData", readAccuralsData);
 
-    // Specify the path to the Excel file on the shared drive
-    const filePath = '/path/to/shared/drive/file.xlsx';
-
-    // Load the workbook
-    const workbook = XLSX.readFile(filePath);
-
-    // Get the first sheet name
-    const sheetName = workbook.SheetNames[0];
-
-    // Get the worksheet
-    const worksheet = workbook.Sheets[sheetName];
-
-    // Convert the worksheet to JSON format
-    const jsonData = XLSX.utils.sheet_to_json(worksheet);
-
-    // Output the JSON data
-    console.log(jsonData);
 });
+const readAccuralsData = async (req, next) => {
+
+    const axios = require('axios');
+    const xlsx = require('xlsx');
+    const spauth = require('node-sp-auth');
+    
+    async function readExcelFileFromSharePoint(url, username, password) {
+      try {
+        // //Obtain CSRF token from SharePoint
+        // const getTokenUrl = `${url}/_api/contextinfo`;
+        // const tokenResponse = await axios.post(getTokenUrl, null, {
+        //   auth: {
+        //     username: username,
+        //     password: password
+        //   }
+        // });
+        let siteUrl = `https://suncor.sharepoint.com/sites/2DayClose/Shared Documents/Forms/AllItems.aspx?RootFolder=/sites/2DayClose` ;
+        const credentials = await spauth.getAuth(siteUrl, {
+            username: username,
+            password: password
+          });
+    
+        //const csrfToken = tokenResponse.data.d.GetContextWebInformation.FormDigestValue;
+    
+        // Read the Excel file from SharePoint with CSRF token
+        //const getFileUrl = `${url}/_api/web/GetFileByServerRelativeUrl('${url}')/$value`;
+        const fileResponse = await axios.get(url, {
+        //   headers: {
+        //     'X-RequestDigest': csrfToken
+        //   },
+          headers: credentials,
+        //   auth: {
+        //     username: username,
+        //     password: password
+        //   },
+          responseType: 'arraybuffer'
+        });
+    
+        const workbook = xlsx.read(fileResponse.data, { type: 'buffer' });
+        const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+        const data = xlsx.utils.sheet_to_json(worksheet);
+    
+        return data;
+      } catch (error) {
+        console.error('Error reading Excel file from SharePoint:', error);
+      }
+    }
+    
+    // Specify the SharePoint file URL and authentication credentials
+    const fileUrl = 'https://suncor.sharepoint.com/:x:/r/sites/2DayClose/_layouts/15/Doc.aspx?sourcedoc=%7B014C19F9-C439-429D-A641-7B1DB7F5B621%7D&file=Copy%20of%20ESSR_working%20file.xlsx';
+    const username = 'skumar@convergentis.com';
+    const password = '8dd48d6a#SK';
+    
+    // Read the Excel file from SharePoint
+    readExcelFileFromSharePoint(fileUrl, username, password)
+      .then(data => {
+        console.log(data);
+      });
+    
+};
